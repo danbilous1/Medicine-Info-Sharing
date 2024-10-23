1b304e4087e2478d66e9abb45bcc9d9b6bb1cb65
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2024-10-23 22:09:40 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -6,33 +6,75 @@ const expiration = document.querySelector('#expiration');
 const comment = document.querySelector('#comment');
 
 let link = '';
+let i = 1;
+let data;
+
+function sendData() {
+  data = {
+    medicine: medicine.value,
+    producer: producer.value,
+    storage: storage.value,
+    expiration: expiration.value,
+    comment: comment.value,
+  };
+
+  fetch('/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data: data }),
+  })
+  .then(res => res.json())
+  .then(result => {
+    link = result.link;
+    
+
+    const container = document.querySelector('.container');
+    const htmlLink = document.createElement('p');
+    htmlLink.innerHTML = `<a href="${link}">Open</a>`;
+    htmlLink.classList.add('link-text');
+    container.appendChild(htmlLink);
+  })
+}
 
 sendBtn.addEventListener('click', function() {
-  
   if (medicine.value && producer.value && storage.value && expiration.value && comment.value) {
-    const data = {
-      medicine: medicine.value,
-      producer: producer.value,
-      storage: storage.value,
-      expiration: expiration.value,
-      comment: comment.value,
-    };
+    if (i >= 2) {
+      if (medicine.value === data.medicine && producer.value === data.producer && storage.value === data.storage && expiration.value === data.expiration && comment.value === data.comment) {
+        alert('You have already sent this data');
+      } else {
+        data = {
+          medicine: medicine.value,
+          producer: producer.value,
+          storage: storage.value,
+          expiration: expiration.value,
+          comment: comment.value,
+        };
 
-    fetch('/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ data: data }),
-    })
-    .then(res => res.json())
-    .then(result => {
-      link = result.link;
+        fetch('/api', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: data }),
+        })
+        .then(res => res.json())
+        .then(result => {
+          link = result.link;
 
-      const htmlLink = document.createElement('p');
-      htmlLink.innerHTML = `<a href="${link}">Invite link</a>`;
-      document.body.appendChild(htmlLink);
-    })
+          document.querySelector('.link-text').remove();
+          const container = document.querySelector('.container');
+          const htmlLink = document.createElement('p');
+          htmlLink.innerHTML = `<a href="${link}">Open</a>`;
+          htmlLink.classList.add('link-text');
+          container.appendChild(htmlLink);
+        })
+      }
+    } else {
+        sendData();
+      i++;
+    }
   } else {
     console.log('type in the inputs');
   }
