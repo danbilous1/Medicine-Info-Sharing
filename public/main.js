7358ed7578a898996a5dae5fa90ed7ba1d71cf3a
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2024-10-30 23:10:33 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -8,6 +8,7 @@ const comment = document.querySelector('#comment');
 let link = '';
 let i = 1;
 let data;
+const alert = document.createElement('div');
 
 function sendData() {
   data = {
@@ -28,21 +29,37 @@ function sendData() {
   .then(res => res.json())
   .then(result => {
     link = result.link;
-    
 
     const container = document.querySelector('.container');
     const htmlLink = document.createElement('p');
+
+    alert.classList.remove('alert-danger');
+    alert.classList.add('alert', 'alert-primary');
+    alert.innerText = 'Link was created.';
     htmlLink.innerHTML = `<a href="${link}">Open</a>`;
     htmlLink.classList.add('link-text');
-    container.appendChild(htmlLink);
-  })
+
+    container.append(alert, htmlLink);
+    setTimeout(() => Delete(alert), 1500);
+  });
+}
+
+function Delete(div) {
+    div.remove();
 }
 
 sendBtn.addEventListener('click', function() {
+  const container = document.querySelector('.container');
+
   if (medicine.value && producer.value && storage.value && expiration.value && comment.value) {
     if (i >= 2) {
       if (medicine.value === data.medicine && producer.value === data.producer && storage.value === data.storage && expiration.value === data.expiration && comment.value === data.comment) {
-        alert('You have already sent this data');
+        console.log(1, i);
+        alert.classList.add('alert', 'alert-danger');
+        alert.innerText = 'You have already sent this data.';
+
+        container.appendChild(alert);
+        setTimeout(() => Delete(alert), 3000);
       } else {
         data = {
           medicine: medicine.value,
@@ -63,19 +80,30 @@ sendBtn.addEventListener('click', function() {
         .then(result => {
           link = result.link;
 
-          document.querySelector('.link-text').remove();
           const container = document.querySelector('.container');
           const htmlLink = document.createElement('p');
+
+          alert.classList.remove('alert-danger');
+          document.querySelector('.link-text').remove();
+          alert.classList.add('alert', 'alert-primary', 'mb-2');
+          alert.innerText = 'Link was created.';
           htmlLink.innerHTML = `<a href="${link}">Open</a>`;
           htmlLink.classList.add('link-text');
-          container.appendChild(htmlLink);
-        })
+
+          container.append(alert, htmlLink);
+          setTimeout(() => Delete(alert), 1500);
+        });
       }
     } else {
-        sendData();
+      console.log(2, i);
+      sendData();
       i++;
     }
   } else {
-    console.log('type in the inputs');
+    alert.classList.add('alert', 'alert-danger');
+    alert.innerText = 'Please full in inputs.';
+
+    container.appendChild(alert);
+    setTimeout(() => Delete(alert), 3000);
   }
 });
