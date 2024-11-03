881dd9a2b454879a5999915fe2813b91cb5bc192
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2024-11-03 11:32:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
 const app = express();
@@ -10,37 +10,59 @@ app.use(express.static("public"));
 const __dirname = path.resolve();
 app.set("index", path.join(__dirname, "index"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const database = {};
 
-app.post('/api', (req, res) => {
+app.post("/api", (req, res) => {
   const { data } = req.body;
 
   if (data && data.medicine) {
     let link = uuidv4();
     let name = uuidv4();
 
-    app.get(`/${link.slice(0,8)}`, (req, res) => {
-      
-      res.sendFile(path.join(__dirname, 'public', 'invite.html'));
+    app.get(`/${link.slice(0, 8)}`, (req, res) => {
+      res.sendFile(path.join(__dirname, "public", "invite.html"));
 
       app.get(`/${link.slice(0, 8)}/data`, (req, res) => {
-        res.json(database[name.slice(0,8)]);
+        res.json(database[name.slice(0, 8)]); // with updated invite, you return only inviteName - comment
       });
-    })
-    
-    database[name.slice(0,8)] = data;
+    });
 
-    res.status(200).json({link: `/${link.slice(0,8)}`, info: database[name.slice(0,8)]});
+    database[name.slice(0, 8)] = data;
+
+    res
+      .status(200)
+      .json({ link: `/${link.slice(0, 8)}`, info: database[name.slice(0, 8)] });
     console.log(database);
   } else {
-    res.status(400).json({ success: false, message: 'No data provided' });
+    res.status(400).json({ success: false, message: "No data provided" });
   }
 });
 
 app.listen(3000, () => {
-  console.log('Express server initialized');
+  console.log("Express server initialized");
 });
+
+// HERE IS HOW INVITE INFO NEEDS TO LOOK:
+// UPDATED INVITE
+// d5693c94: {
+//     inviteName: 'd5693c94',
+//     medicine: 'd',
+//     producer: 'd',
+//     storage: 'd',
+//     expiration: 'd',
+//     comment: 'd',
+//     password: 'random generated with uuid',
+//   }
+
+// OLD INVITE
+// d5693c94: {
+//     medicine: 'd',
+//     producer: 'd',
+//     storage: 'd',
+//     expiration: 'd',
+//     comment: 'd'
+//   }
