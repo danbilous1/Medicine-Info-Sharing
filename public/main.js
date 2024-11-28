7f30c77a083fac670698b9efa1ebac25e5b35d45
Dan Bilous <104320090+danbilous1@users.noreply.github.com>
2024-11-28 19:17:43 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ const producer = document.querySelector("#producer");
 const storage = document.querySelector("#storage");
 const expiration = document.querySelector("#expiration");
 const comment = document.querySelector("#comment");
+const container = document.querySelector(".container");
 
 let link = "";
 let i = 1;
@@ -38,61 +39,63 @@ function sendData() {
   })
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
-      link = result.link;
-      medicine.value = "";
-      producer.value = "";
-      storage.value = "";
-      expiration.value = "";
-      comment.value = "";
+      if (result == false) {
+        alert.classList.add("alert", "alert-danger");
+        alert.innerText = "Inputs are empty or non-valid";
 
-      const container = document.querySelector(".container");
-      const sendContainer = document.querySelector(".send-container");
-      const passContainer = document.querySelector(".pass-container");
+        container.appendChild(alert);
+        setTimeout(() => Delete(alert), 3000);
+      } else {
+        link = result.link;
+        medicine.value = "";
+        producer.value = "";
+        storage.value = "";
+        expiration.value = "";
+        comment.value = "";
 
-      const htmlLink = document.createElement("p");
-      const password = document.querySelector(".pass");
-      const showBtn = document.createElement("button");
-      const copyBtn = document.createElement("button");
+        const sendContainer = document.querySelector(".send-container");
+        const passContainer = document.querySelector(".pass-container");
 
-      let passwordData = result.pass;
+        const htmlLink = document.createElement("p");
+        const password = document.querySelector(".pass");
+        const showBtn = document.createElement("button");
+        const copyBtn = document.createElement("button");
 
-      password.innerText = "Editing Password: *********";
-      showBtn.innerText = "Show";
-      showBtn.classList.add("btn", "btn-outline-dark");
-      copyBtn.innerText = "Copy to Clipboard";
-      copyBtn.classList.add("btn", "btn-dark");
+        let passwordData = result.pass;
 
-      function clear(el, text) {
-        el.innerText = text;
-      }
+        password.innerText = "Editing Password: *********";
+        showBtn.innerText = "Show";
+        showBtn.classList.add("btn", "btn-outline-dark");
+        copyBtn.innerText = "Copy to Clipboard";
+        copyBtn.classList.add("btn", "btn-dark");
 
-      let check = true;
-      showBtn.addEventListener("click", function () {
-        if (check) {
-          password.innerText = "Editing Password: " + passwordData;
-          check = false;
-        } else {
-          password.innerText = "Editing Password: *********";
-          check = true;
+        function clear(el, text) {
+          el.innerText = text;
         }
-      });
-      copyBtn.addEventListener("click", function () {
-        navigator.clipboard.writeText("Your Editing Password: " + passwordData);
-        copyBtn.innerText = "Copied to Clipboard";
-        setTimeout(() => clear(copyBtn, "Copy to Clipboard"), 1500);
-      });
 
-      alert.classList.remove("alert-danger");
-      alert.classList.add("alert", "alert-primary", "mt-4");
-      alert.innerText = "Link was created.";
-      htmlLink.innerHTML = `<a href="${link}">Open Invite Link</a>`;
-      htmlLink.classList.add("link-text", "mt-2");
+        let check = true;
+        showBtn.addEventListener("click", function () {
+          if (check) {
+            password.innerText = "Editing Password: " + passwordData;
+            check = false;
+          } else {
+            password.innerText = "Editing Password: *********";
+            check = true;
+          }
+        });
+        copyBtn.addEventListener("click", function () {
+          navigator.clipboard.writeText(
+            "Your Editing Password: " + passwordData
+          );
+          copyBtn.innerText = "Copied to Clipboard";
+          setTimeout(() => clear(copyBtn, "Copy to Clipboard"), 1500);
+        });
+        htmlLink.innerHTML = `<a href="${link}">Open Invite Link</a>`;
+        htmlLink.classList.add("link-text", "mt-2");
 
-      sendContainer.appendChild(htmlLink);
-      container.append(alert);
-      passContainer.append(showBtn, copyBtn);
-      setTimeout(() => Delete(alert), 1500);
+        sendContainer.appendChild(htmlLink);
+        passContainer.append(showBtn, copyBtn);
+      }
     });
 }
 
@@ -102,72 +105,58 @@ function Delete(div) {
 
 sendBtn.addEventListener("click", function () {
   const container = document.querySelector(".container");
+  if (i >= 2) {
+    if (
+      medicine.value === data.medicine &&
+      producer.value === data.producer &&
+      storage.value === data.storage &&
+      expiration.value === data.expiration &&
+      comment.value === data.comment
+    ) {
+      console.log(1, i);
+      alert.classList.add("alert", "alert-danger", "mt-4");
+      alert.innerText = "You have already sent this data.";
 
-  if (
-    medicine.value &&
-    producer.value &&
-    storage.value &&
-    expiration.value &&
-    comment.value
-  ) {
-    if (i >= 2) {
-      if (
-        medicine.value === data.medicine &&
-        producer.value === data.producer &&
-        storage.value === data.storage &&
-        expiration.value === data.expiration &&
-        comment.value === data.comment
-      ) {
-        console.log(1, i);
-        alert.classList.add("alert", "alert-danger", "mt-4");
-        alert.innerText = "You have already sent this data.";
+      container.appendChild(alert);
+      setTimeout(() => Delete(alert), 3000);
+    } else {
+      data = {
+        medicine: medicine.value,
+        producer: producer.value,
+        storage: storage.value,
+        expiration: expiration.value,
+        comment: comment.value,
+      };
 
-        container.appendChild(alert);
-        setTimeout(() => Delete(alert), 3000);
-      } else {
-        data = {
-          medicine: medicine.value,
-          producer: producer.value,
-          storage: storage.value,
-          expiration: expiration.value,
-          comment: comment.value,
-        };
-
-        fetch("/api", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ data: data }),
-        })
-          .then((res) => res.json())
-          .then((result) => {
+      fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: data }),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          if (result == false) {
+            console.log("dddd");
+            alert.classList.add("alert", "alert-danger", "mt-4");
+            alert.innerText = "Inputs are empty or non-valid.";
+            container.appendChild(alert);
+            setTimeout(() => Delete(alert), 3000);
+          } else {
             link = result.link;
-
-            const container = document.querySelector(".container");
             const htmlLink = document.createElement("p");
-
-            alert.classList.remove("alert-danger");
             document.querySelector(".link-text").remove();
-            alert.classList.add("alert", "alert-primary", "mb-2");
-            alert.innerText = "Link was created.";
             htmlLink.innerHTML = `<a href="${link}">Open</a>`;
             htmlLink.classList.add("link-text");
 
-            container.append(alert, htmlLink);
-            setTimeout(() => Delete(alert), 1500);
-          });
-      }
-    } else {
-      console.log(2, i);
-      sendData();
-      i++;
+            container.append(htmlLink);
+          }
+        });
     }
   } else {
-    alert.classList.add("alert", "alert-danger");
-    alert.innerText = "Please full in inputs.";
-
-    container.appendChild(alert);
-    setTimeout(() => Delete(alert), 3000);
+    console.log(2, i);
+    sendData();
+    i++;
   }
 });
